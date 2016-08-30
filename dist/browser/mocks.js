d93970aba8f4b69f9316a00965ec22e8183a9e22
Alexander J. Vincent <ajvincent@gmail.com>
2016-08-29 23:49:51 -0700

    Whitespace indentation applies to distribution files too.

--- dist/browser/mocks.js
+++ dist/browser/mocks.js
@@ -289,22 +289,22 @@ var ElementDry, NodeDry, dryDocument;
   assert(found, "NodeDry not found as a proxy!");
 }
 function dampObjectGraph(parts) {
-    parts.handlers = {
-        "wet":  parts.membrane.getHandlerByField("wet"),
-        "dry":  parts.membrane.getHandlerByField("dry"),
-        "damp": parts.membrane.getHandlerByField("damp"),
-    };
+  parts.handlers = {
+    "wet":  parts.membrane.getHandlerByField("wet"),
+    "dry":  parts.membrane.getHandlerByField("dry"),
+    "damp": parts.membrane.getHandlerByField("damp"),
+  };
 
-    let keys = Object.getOwnPropertyNames(parts.wet);
-    parts.damp = {};
-    for (let i = 0; i < keys.length; i++) {
-        let key = keys[i];
-        parts.damp[key] = parts.membrane.convertArgumentToProxy(
-            parts.handlers.wet,
-            parts.handlers.damp,
-            parts.wet[key]
-        );
-    }
+  let keys = Object.getOwnPropertyNames(parts.wet);
+  parts.damp = {};
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i];
+    parts.damp[key] = parts.membrane.convertArgumentToProxy(
+      parts.handlers.wet,
+      parts.handlers.damp,
+      parts.wet[key]
+    );
+  }
 }
   // The bare essentials.
   var Mocks = {
