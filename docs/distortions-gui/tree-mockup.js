39a4eda05028fc5423484ab9cbf3fd959d15a5db
Alexander J. Vincent <ajvincent@gmail.com>
2017-09-03 21:27:17 -0700

    #90 prelude, move cells inside rows when display: contents is available

--- docs/distortions-gui/tree-mockup.js
+++ docs/distortions-gui/tree-mockup.js
@@ -1,6 +1,19 @@
 function styleAndMoveTreeColumns(gridtree) {
   "use strict";
-  const sheet = getCustomStylesheet(), inputMap = new Map();
+  {
+    let pass = false;
+    try {
+      if (Boolean(CSS) && (typeof CSS.supports === "function")) 
+        pass = CSS.supports("display", "grid");
+    }
+    catch (e) {
+      // do nothing
+    }
+    if (!pass)
+      return;
+  }
+
+  const sheet = getCustomStylesheet(gridtree.ownerDocument);
 
   // Step 1:  Mark all the ul elements as a row block.
   {
@@ -53,86 +66,156 @@ function styleAndMoveTreeColumns(gridtree) {
       }
     }
   }
-
-  // the second span in each listitem
-  const listitemMap = new Map();
-
-  // Step 4:  Assign class names to li > span:not(first-child) elements and append them to the grid.
+  
+  /*
+  if (CSS.supports("display", "subgrid"))
+    gridtree.setAttribute("rowdisplay", "subgrid");
+  else
+  */
+  if (CSS.supports("display", "contents"))
   {
-    let spanList = gridtree.getElementsByTagName("span");
-    for (let i = 0; i < spanList.length; i++) {
-      let span = spanList[i], parent = span.parentNode;
-      if (parent.nodeName.toLowerCase() != "li")
-        continue;
-      if (span.previousElementSibling === null)
-        continue;
+    gridtree.setAttribute("cellgrouping", "row-contents");
 
-      span.setAttribute("row", parent.getAttribute("row"));
-      if (parent.classList.contains("header"))
-        span.classList.add("header");
-
-      // Beyond this point, we are committed.
-      i--;
-      let ul = parent.parentNode;
-      do {
-        let className = "rowblock-" + ul.getAttribute("rowblock");
-        span.classList.add(className);
-        if (ul.parentNode.classList.contains("treeroot"))
-          break;
-        ul = ul.parentNode.parentNode;
-      } while (true);
-      gridtree.appendChild(span);
-
-      if (!listitemMap.has(parent))
-        listitemMap.set(parent, span);
-    }
-  }
-
-  // Step 5:  Move li contents to the grid.
-  {
+    // Step 4:  Move all contents of each li (excluding ul) to a new row.
     let items = gridtree.getElementsByTagName("li");
     for (let i = 0; i < items.length; i++) {
-      let item = items[i],
-          span = document.createElement("span"),
-          checkSpan = document.createElement("span"),
-          child;
-      span.setAttribute("row", item.getAttribute("row"));
+      let item = items[i], spanCount = 0;
+
+      let row = document.createElement("div");
+      row.setAttribute("row", item.getAttribute("row"));
       if (item.classList.contains("header"))
-        span.classList.add("header");
-      span.setAttribute("firstCol", "true");
+        row.classList.add("header");
 
-      while ((child = item.firstElementChild)) {
-        if (child.nodeName.toLowerCase() === "ul")
-          break;
-        
-        if (child.classList.contains("collapsible-check") &&
-            item.classList.contains("collapsible") &&
-            !checkSpan.parentNode) {
-          span.insertBefore(checkSpan, span.firstChild);
-          span.insertBefore(child, checkSpan);
-          continue;
-        }
-
-        span.appendChild(child);
-      }
 
       let ul = item.parentNode, indent = 0;
       do {
         let className = "rowblock-" + ul.getAttribute("rowblock");
-        span.classList.add(className);
+        if (ul.parentNode.classList.contains("collapsible"))
+          row.classList.add(className);
         if (ul.parentNode.classList.contains("treeroot"))
           break;
         ul = ul.parentNode.parentNode;
         indent++;
       } while (true);
-      span.setAttribute("indent", indent);
 
-      let refChild = listitemMap.get(item);
-      gridtree.insertBefore(span, refChild);
-      listitemMap.delete(item);
+      row.setAttribute("indent", indent);
+      gridtree.appendChild(row);
+
+      while (true) {
+        let elem = item.firstElementChild;
+        if (elem === null)
+          break;
+        let name = elem.localName.toLowerCase();
+        if (name === "ul")
+          break;
+        if (name === "span")
+        {
+          spanCount++;
+          if (spanCount === 1) {
+            let firstSpan = document.createElement("span");
+            row.appendChild(firstSpan);
+            firstSpan.appendChild(elem);
+          }
+          else
+            row.appendChild(elem);
+        }
+        else if ((name === "input") &&
+                 (spanCount === 1) &&
+                 elem.classList.contains("collapsible-check") &&
+                 item.classList.contains("collapsible"))
+        {
+          row.firstChild.insertBefore(elem, row.firstChild.firstChild);
+        }
+        else
+          row.appendChild(elem);
+      }
+
     }
   }
+  else {
+    gridtree.setAttribute("cellgrouping", "none");
 
-  // Step 6:  Hide the tree, since it no longer contains useful information.
+    // the second span in each listitem
+    const listitemMap = new Map();
+  
+    // Step 4:  Assign class names to li > span:not(first-child) elements and append them to the grid.
+    {
+      let spanList = gridtree.getElementsByTagName("span");
+      for (let i = 0; i < spanList.length; i++) {
+        let span = spanList[i], parent = span.parentNode;
+        if (parent.nodeName.toLowerCase() != "li")
+          continue;
+        if (span.previousElementSibling === null)
+          continue;
+  
+        span.setAttribute("row", parent.getAttribute("row"));
+        if (parent.classList.contains("header"))
+          span.classList.add("header");
+  
+        // Beyond this point, we are committed.
+        i--;
+        let ul = parent.parentNode;
+        do {
+          let className = "rowblock-" + ul.getAttribute("rowblock");
+          span.classList.add(className);
+          if (ul.parentNode.classList.contains("treeroot"))
+            break;
+          ul = ul.parentNode.parentNode;
+        } while (true);
+        gridtree.appendChild(span);
+  
+        if (!listitemMap.has(parent))
+          listitemMap.set(parent, span);
+      }
+    }
+  
+    // Step 5:  Move li contents to the grid.
+    {
+      let items = gridtree.getElementsByTagName("li");
+      for (let i = 0; i < items.length; i++) {
+        let item = items[i],
+            span = document.createElement("span"),
+            checkSpan = document.createElement("span"),
+            child;
+        span.setAttribute("row", item.getAttribute("row"));
+        if (item.classList.contains("header"))
+          span.classList.add("header");
+        span.setAttribute("firstCol", "true");
+  
+        while ((child = item.firstElementChild)) {
+          if (child.nodeName.toLowerCase() === "ul")
+            break;
+          
+          if (child.classList.contains("collapsible-check") &&
+              item.classList.contains("collapsible") &&
+              !checkSpan.parentNode) {
+            span.insertBefore(checkSpan, span.firstChild);
+            span.insertBefore(child, checkSpan);
+            continue;
+          }
+  
+          span.appendChild(child);
+        }
+  
+        let ul = item.parentNode, indent = 0;
+        do {
+          let className = "rowblock-" + ul.getAttribute("rowblock");
+          span.classList.add(className);
+          if (ul.parentNode.classList.contains("treeroot"))
+            break;
+          ul = ul.parentNode.parentNode;
+          indent++;
+        } while (true);
+        span.setAttribute("indent", indent);
+  
+        let refChild = listitemMap.get(item);
+        gridtree.insertBefore(span, refChild);
+        listitemMap.delete(item);
+      }
+    }
+
+  }
+
+  // Final step:  Remove the tree, since it no longer contains useful information.
   gridtree.removeChild(gridtree.firstElementChild);
 }
