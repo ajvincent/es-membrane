7ba590763d20fba6ba8607e6ddf8e8e3079d2ff2
qk8333 <evan.smith.m@gmail.com>
2017-09-28 19:27:28 -0700

    #100, multistate.js update

--- docs/gui/multistate/multistate.js
+++ docs/gui/multistate/multistate.js
@@ -1,9 +1,13 @@
-document.getElementsByClassName("multistate")[0].disabled = false;
-
-document.getElementsByClassName("multistate")[0].onclick = function() {
-  var multistate = document.getElementsByClassName("multistate")[0];
+function MultistateHandler(event) {
+  const multistate = event.target;
   multistate.appendChild(multistate.firstElementChild);
-  multistate.append("\n");
-  multistate.removeChild(multistate.firstChild);
-  multistate.parentNode.getElementsByTagName("input")[0].disabled = (multistate.firstElementChild.dataset.state != "number");
+  multistate.value = multistate.firstElementChild.dataset.state;
 }
+
+/*
+{
+  let buttons = document.getElementsByClassName("multistate");
+  for (let i = 0; i < buttons.length; i++)
+    buttons[i].addEventListener("click", MultistateHandler, true);
+}
+*/
