eeaf9891398b8f85a90f000c49c027fd6dd109c9
Alexander J. Vincent <ajvincent@gmail.com>
2017-12-27 10:08:07 -0800

    No ticket, reduce "karma concurrency level" to 1.

--- karma.conf.js
+++ karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: 1
   };
 
   if (process.env.TRAVIS) {
