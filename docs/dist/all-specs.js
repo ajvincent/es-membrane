e2a4f252a7cc2e119a838e06741127fef8d6af05
Alexander J. Vincent <ajvincent@gmail.com>
2018-07-01 00:23:42 -0700

    #65 add membrane tests illustrating the weird behavior, for TC39 talk.

--- docs/dist/all-specs.js
+++ docs/dist/all-specs.js
@@ -10809,6 +10809,13 @@ describe("TC39 demonstrations of Array objects in membranes: ", function() {
 
       populateDry();
 
+      /* At this point, parts.dry.array has never seen delta or epsilon.  The
+       * proxy has explicitly been told that new properties should stay local to
+       * the proxy and not propagate through... but the splice method belongs
+       * to the wet object graph, so execution takes place there and the
+       * membrane ignores the storeUnknownAsLocal setting for the array.
+       */
+
       parts.dry.array.splice(1, 1, parts.dry.delta, parts.dry.epsilon);
       expect(parts.wet.array).toEqual([
         parts.wet.alpha,
@@ -10826,85 +10833,157 @@ describe("TC39 demonstrations of Array objects in membranes: ", function() {
     }
   );
 
-  xit(
-    "Array.prototype.splice on the untrusted graph doesn't work as expected either",
+  it(
+    "Array.prototype.splice behaves oddly with storeUnknownAsLocal",
     function() {
-      /*
-      logger = loggerLib.getLogger("test.membrane.tc39.arrays");
-      const appender = new loggerLib.Appender();
-      logger.addAppender(appender);
+      let appender, chain;
 
-      const chain = parts.membrane.modifyRules.createChainHandler(
-        parts.handlers.dry
-      );
-      parts.membrane.allTraps.forEach(function(trap) {
-        chain[trap] = function() {
-          logger.info(trap + " enter");
-          const rv = this.nextHandler[trap].apply(this, arguments);
-          logger.info(trap + " leave");
-          return rv;
-        };
-      });
-      ["get", "getOwnPropertyDescriptor", "set", "defineProperty"].forEach(function(trap) {
-        chain[trap] = function(target, propertyName) {
-          try {
-            logger.info(trap + " enter " + propertyName);
-          }
-          catch (e) {
+      // this is all boilerplate for internal debugging
+      if (false) {
+        logger = loggerLib.getLogger("test.membrane.tc39.arrays");
+        appender = new loggerLib.Appender();
+        logger.addAppender(appender);
+
+        chain = parts.membrane.modifyRules.createChainHandler(
+          parts.handlers.dry
+        );
+        parts.membrane.allTraps.forEach(function(trap) {
+          chain[trap] = function() {
             logger.info(trap + " enter");
-          }
-          const rv = this.nextHandler[trap].apply(this, arguments);
-          try {
-            logger.info(trap + " leave " + propertyName);
-          }
-          catch (e) {
+            const rv = this.nextHandler[trap].apply(this, arguments);
             logger.info(trap + " leave");
-          }
-          return rv;
-        };
-      });
+            return rv;
+          };
+        });
 
-      let pass = false;
+        ["get", "getOwnPropertyDescriptor"].forEach(function(trap) {
+          chain[trap] = function(target, propertyName) {
+            try {
+              logger.info(trap + " enter " + propertyName);
+            }
+            catch (e) {
+              logger.info(trap + " enter");
+            }
+            const rv = this.nextHandler[trap].apply(this, arguments);
+            try {
+              logger.info(trap + " leave " + propertyName);
+            }
+            catch (e) {
+              logger.info(trap + " leave");
+            }
+            return rv;
+          };
+        });
+
+        ["set", "defineProperty"].forEach(function(trap) {
+          chain[trap] = function(target, propertyName) {
+            const hasDesc = Boolean(
+              this.nextHandler.getOwnPropertyDescriptor(target, propertyName)
+            );
+            try {
+              logger.info(`${trap} enter ${propertyName} (has: ${hasDesc})`);
+            }
+            catch (e) {
+              logger.info(`${trap} enter (has: ${hasDesc})`);
+            }
+            const rv = this.nextHandler[trap].apply(this, arguments);
+            try {
+              logger.info(trap + " leave " + propertyName);
+            }
+            catch (e) {
+              logger.info(trap + " leave");
+            }
+            return rv;
+          };
+        });
+      }
 
       parts.handlers.dry.addProxyListener(function(meta) {
         if (meta.target !== parts.wet.array)
           return;
         try {
           parts.membrane.modifyRules.storeUnknownAsLocal("dry", meta.proxy);
-          parts.membrane.modifyRules.replaceProxy(meta.proxy, chain);
-          pass = true;
+          if (chain)
+            parts.membrane.modifyRules.replaceProxy(meta.proxy, chain);
         }
         catch (e) {
           meta.throwException(e);
         }
       });
-      */
-
       populateDry();
-      /*
-      expect(pass).toBe(true);
-      appender.clear();
-      */
+
+      // debugging
+      if (appender)
+        appender.clear();
+
+      /* [ alpha, beta, gamma ] */
+
+      /* At this point, parts.dry.array has never seen delta or epsilon.  The
+       * proxy has explicitly been told that new properties should stay local to
+       * the proxy and not propagate through...
+       *
+       * This time, we use splice.apply to simulate running splice in the dry
+       * object graph, which is about to cause its own problems.
+       */
 
       Array.prototype.splice.apply(parts.dry.array, [
         1, 1, parts.dry.delta, parts.dry.epsilon
       ]);
 
-      /* XXX ajvincent Actually, I don't know what to expect.
-      expect(parts.wet.array).toEqual([
-        parts.wet.alpha,
-        parts.wet.delta,
-        parts.wet.epsilon,
-        parts.wet.gamma
-      ]);
+      expect(parts.dry.array.length).toBe(4);
+      expect(parts.dry.array[0]).toBe(parts.dry.alpha);
+      expect(parts.dry.array[1]).toBe(parts.dry.delta);
+      expect(parts.dry.array[2]).toBe(parts.dry.epsilon);
+      expect(parts.dry.array[3]).toBe(parts.dry.gamma);
 
-      expect(parts.dry.array).toEqual([
-        parts.dry.alpha,
-        parts.dry.delta,
-        parts.dry.epsilon,
-        parts.dry.gamma
-      ]);
-      */
+      expect(parts.wet.array.length).toBe(4);
+      expect(parts.wet.array[0]).toBe(parts.wet.alpha);
+
+      expect(parts.wet.array.includes(parts.wet.gamma)).toBe(false);
+
+      /* XXX ajvincent This is actually pretty bad.  The same operation on the
+       * wet graph would have resulted in [ alpha, delta, epsilon, gamma ].
+       *
+       * At the same time, I don't believe this is, strictly speaking, a bug in
+       * the membrane implementation.  Normally, when I have a test that shows
+       * buggy behavior, I tend to think either the implementation is broken,
+       * the test is broken, both, or that there's a flaw in the design.
+       *
+       * This shows something different:  I'm using an API in a way that it was
+       * never designed to be used.  I'm performing a distortion on an array
+       * (storeUnknownAsLocal), and then doing a simple splice on it.
+       *
+       * With a distortion on an array, typically there would be three desirable
+       * outcomes to choose from:
+       *
+       * (1) Holes in the array, when we're hiding something
+       * (2) Explicitly undefined spaces in the array, when we're hiding something
+       * (3) A continuous list of elements, maintaining order and consistency.
+       *
+       * Here, we've violated that.  The gamma element was in the array before
+       * the splice.  The splice shouldn't have removed it, but it did.
+       *
+       * This comes down to the algorithm of the splice method as it's
+       * defined in the ECMAScript specification.  In particular, it does the
+       * rearranging and insertion of elements in-place.  The "this" argument of
+       * the splice method is a Proxy that moves the setting of unknown
+       * properties to the proxy only - leaving them out of the unwrapped array.
+       *
+       * Specifically, `this[3] = parts.dry.gamma` is a step that happens inside
+       * the splice algorithm here, but that means parts.wet.gamma[3] is not
+       * correspondingly set.  Then later, when this[1] and this[2] are also set
+       * using the Proxy API, the reference to parts.wet.gamma is permanently
+       * lost from the wet array.
+       *
+       * So yes, we've created a hole in the wet array (which is acceptable, if
+       * ugly), but we've also created data-loss (which is not acceptable).
+       *
+       * Individually though, each of the pieces is working as it was originally
+       * intended:  splice is very specifically designed, as is the Proxy API,
+       * and storeUnknownAsLocal is simply forcing the proxy to keep "new" values
+       * private.  It's the combination that's flawed, and the combination that
+       * needs special handling.
+       */
     }
   );
 });
