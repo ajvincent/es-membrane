12ef686c6b55814f4b5ce4ecd94c1ba988c7034a
Alexander J. Vincent <ajvincent@gmail.com>
2019-11-18 22:01:25 -0800

    #200, LinkedList handler unit tests for proxies.

--- docs/dist/all-specs.js
+++ docs/dist/all-specs.js
@@ -2944,6 +2944,9 @@ describe("MembraneProxyHandlers.LinkedList proxy handler", function() {
     shadow = {};
     mirror = {};
     handler = new MembraneProxyHandlers.LinkedList({membrane: null});
+    let obj = Proxy.revocable(shadow, handler);
+    proxy = obj.proxy;
+    revoke = obj.revoke;
   });
 
   afterEach(function() {
@@ -2967,26 +2970,71 @@ describe("MembraneProxyHandlers.LinkedList proxy handler", function() {
       function base() {}
       shadow = new base();
       mirror = new base();
-      expect(handler.getPrototypeOf(shadow)).toBe(Reflect.getPrototypeOf(mirror));
+
+      revoke();
+      let obj = Proxy.revocable(shadow, handler);
+      proxy = obj.proxy;
+      revoke = obj.revoke;
+
+      const expected = Reflect.getPrototypeOf(mirror);
+      let shadowActual = handler.getPrototypeOf(shadow);
+      expect(shadowActual).toBe(expected);
+
+      let proxyActual = Reflect.getPrototypeOf(proxy);
+      expect(proxyActual).toBe(expected);
     });
 
-    it("on the setPrototypeOf trap", function() {
+    it("on the setPrototypeOf trap (via the shadow)", function() {
       function base() {}
       shadow = new base();
       const proto = {};
+
+      revoke();
+      let obj = Proxy.revocable(shadow, handler);
+      proxy = obj.proxy;
+      revoke = obj.revoke;
+
       handler.setPrototypeOf(shadow, proto);
       expect(Reflect.getPrototypeOf(shadow)).toBe(proto);
+      expect(Reflect.getPrototypeOf(proxy)).toBe(proto);
     });
 
-    it("on the isExtensible trap", function() {
+    it("on the setPrototypeOf trap (via the proxy)", function() {
+      function base() {}
+      shadow = new base();
+      const proto = {};
+
+      revoke();
+      let obj = Proxy.revocable(shadow, handler);
+      proxy = obj.proxy;
+      revoke = obj.revoke;
+
+      Reflect.setPrototypeOf(proxy, proto);
+      expect(Reflect.getPrototypeOf(shadow)).toBe(proto);
+
+      expect(Reflect.getPrototypeOf(proxy)).toBe(proto);
+    });
+
+    it("on the isExtensible trap (via the shadow)", function() {
       expect(handler.isExtensible(shadow)).toBe(true);
+      expect(Reflect.isExtensible(proxy)).toBe(true);
       Reflect.preventExtensions(shadow);
       expect(handler.isExtensible(shadow)).toBe(false);
+      expect(Reflect.isExtensible(proxy)).toBe(false);
     });
 
-    it("on the preventExtensions trap", function() {
+    it("on the isExtensible trap (via the proxy)", function() {
+      expect(handler.isExtensible(shadow)).toBe(true);
+      expect(Reflect.isExtensible(proxy)).toBe(true);
+      Reflect.preventExtensions(proxy);
+      expect(handler.isExtensible(shadow)).toBe(false);
+      expect(Reflect.isExtensible(proxy)).toBe(false);
+    });
+
+    it("on the preventExtensions trap (via the shadow)", function() {
       handler.preventExtensions(shadow);
       expect(Reflect.isExtensible(shadow)).toBe(false);
+      expect(handler.isExtensible(proxy)).toBe(false);
     });
 
     it("on the getOwnPropertyDescriptor trap", function() {
@@ -2997,14 +3045,23 @@ describe("MembraneProxyHandlers.LinkedList proxy handler", function() {
         configurable: false
       };
       Reflect.defineProperty(shadow, "foo", desc);
+
       const desc2 = handler.getOwnPropertyDescriptor(shadow, "foo");
-      expect(Reflect.ownKeys(desc)).toEqual(Reflect.ownKeys(desc2));
+
+      expect(Reflect.ownKeys(desc2)).toEqual(Reflect.ownKeys(desc));
       Reflect.ownKeys(desc).forEach((key) =>
         expect(desc2[key]).toBe(desc[key])
       );
+
+      const desc3 = Reflect.getOwnPropertyDescriptor(proxy, "foo");
+
+      expect(Reflect.ownKeys(desc3)).toEqual(Reflect.ownKeys(desc));
+      Reflect.ownKeys(desc).forEach((key) =>
+        expect(desc3[key]).toBe(desc[key])
+      );
     });
 
-    it("on the defineProperty trap", function() {
+    it("on the defineProperty trap (via the shadow)", function() {
       const desc = {
         value: {},
         writable: true,
@@ -3012,10 +3069,41 @@ describe("MembraneProxyHandlers.LinkedList proxy handler", function() {
         configurable: false
       };
       handler.defineProperty(shadow, "foo", desc);
+
       const desc2 = Reflect.getOwnPropertyDescriptor(shadow, "foo");
       expect(Reflect.ownKeys(desc)).toEqual(Reflect.ownKeys(desc2));
       Reflect.ownKeys(desc).forEach((key) =>
         expect(desc2[key]).toBe(desc[key])
+      );
+
+      const desc3 = Reflect.getOwnPropertyDescriptor(proxy, "foo");
+
+      expect(Reflect.ownKeys(desc3)).toEqual(Reflect.ownKeys(desc));
+      Reflect.ownKeys(desc).forEach((key) =>
+        expect(desc3[key]).toBe(desc[key])
+      );
+    });
+
+    it("on the defineProperty trap (via the proxy)", function() {
+      const desc = {
+        value: {},
+        writable: true,
+        enumerable: true,
+        configurable: false
+      };
+      Reflect.defineProperty(proxy, "foo", desc);
+
+      const desc2 = Reflect.getOwnPropertyDescriptor(shadow, "foo");
+      expect(Reflect.ownKeys(desc)).toEqual(Reflect.ownKeys(desc2));
+      Reflect.ownKeys(desc).forEach((key) =>
+        expect(desc2[key]).toBe(desc[key])
+      );
+
+      const desc3 = Reflect.getOwnPropertyDescriptor(proxy, "foo");
+
+      expect(Reflect.ownKeys(desc3)).toEqual(Reflect.ownKeys(desc));
+      Reflect.ownKeys(desc).forEach((key) =>
+        expect(desc3[key]).toBe(desc[key])
       );
     });
 
@@ -3026,6 +3114,9 @@ describe("MembraneProxyHandlers.LinkedList proxy handler", function() {
       shadow.bar = {};
       expect(handler.has(shadow, "foo")).toBe(true);
       expect(handler.has(shadow, "bar")).toBe(true);
+
+      expect(Reflect.has(proxy, "foo")).toBe(true);
+      expect(Reflect.has(proxy, "bar")).toBe(true);
     });
 
     it("on the get trap", function() {
@@ -3035,48 +3126,112 @@ describe("MembraneProxyHandlers.LinkedList proxy handler", function() {
       shadow.bar = bar;
       expect(handler.get(shadow, "foo")).toBe(foo);
       expect(handler.get(shadow, "bar")).toBe(bar);
+
+      expect(Reflect.get(proxy, "foo")).toBe(foo);
+      expect(Reflect.get(proxy, "bar")).toBe(bar);
     });
 
-    it("on the set trap", function() {
+    it("on the set trap (via the shadow)", function() {
       const base = {}, foo = {}, bar = {};
       Reflect.setPrototypeOf(shadow, base);
       expect(handler.set(base, "foo", foo)).toBe(Reflect.set(mirror, "foo", foo));
       expect(handler.set(shadow, "bar", bar)).toBe(Reflect.set(mirror, "bar", bar));
+
       expect(Reflect.get(shadow, "foo")).toBe(foo);
       expect(Reflect.get(shadow, "bar")).toBe(bar);
+
+      expect(Reflect.get(proxy, "foo")).toBe(foo);
+      expect(Reflect.get(proxy, "bar")).toBe(bar);
     });
 
-    it("on the deleteProperty trap", function() {
+    it("on the set trap (via the Proxy)", function() {
+      const base = {}, foo = {}, bar = {};
+      Reflect.setPrototypeOf(shadow, base);
+      expect(Reflect.set(base, "foo", foo)).toBe(Reflect.set(mirror, "foo", foo));
+      expect(Reflect.set(proxy, "bar", bar)).toBe(Reflect.set(mirror, "bar", bar));
+
+      expect(Reflect.get(shadow, "foo")).toBe(foo);
+      expect(Reflect.get(shadow, "bar")).toBe(bar);
+
+      expect(Reflect.get(proxy, "foo")).toBe(foo);
+      expect(Reflect.get(proxy, "bar")).toBe(bar);
+    });
+
+    it("on the deleteProperty trap (via the shadow)", function() {
       const foo = {};
       shadow.foo = foo;
       mirror.foo = foo;
       expect(handler.deleteProperty(shadow, "foo")).toBe(Reflect.deleteProperty(mirror, "foo"));
+
       expect(Reflect.hasOwnProperty(shadow, "foo")).toBe(false);
+      expect(Reflect.hasOwnProperty(proxy, "foo")).toBe(false);
+    });
+
+    it("on the deleteProperty trap (via the proxy)", function() {
+      const foo = {};
+      shadow.foo = foo;
+      mirror.foo = foo;
+      expect(Reflect.deleteProperty(proxy, "foo")).toBe(Reflect.deleteProperty(mirror, "foo"));
+
+      expect(Reflect.hasOwnProperty(shadow, "foo")).toBe(false);
+      expect(Reflect.hasOwnProperty(proxy, "foo")).toBe(false);
     });
 
     it("on the ownKeys trap", function() {
       shadow.foo = 1;
       shadow.bar = 2;
-      expect(handler.ownKeys(shadow)).toEqual(Reflect.ownKeys(shadow));
+      const expected = Reflect.ownKeys(shadow);
+      expect(handler.ownKeys(shadow)).toEqual(expected);
+      expect(Reflect.ownKeys(proxy)).toEqual(expected);
     });
 
     it("on the apply trap", function() {
-      const rv1 = Reflect.apply(''.charAt, 'ponies', [3]);
-      const rv2 = handler.apply(''.charAt, 'ponies', [3]);
-      expect(rv1).toBe(rv2);
+      function returner() {
+        expect(this).toBe(expectedThis);
+        expect(Array.from(arguments)).toEqual(expectedArgs);
+        return rv;
+      }
+
+      const rv = {}, expectedArgs = [ "foo", "bar" ];
+      let expectedThis = null;
+
+      mirror.foo = returner;
+      expectedThis = mirror;
+      const rv1 = Reflect.apply(returner, mirror, expectedArgs.slice(0));
+      expect(rv1).toBe(rv);
+
+      shadow.foo = returner;
+      expectedThis = shadow;
+      const rv2 = handler.apply(returner, shadow, expectedArgs.slice(0));
+      expect(rv2).toBe(rv);
+
+      expectedThis = proxy;
+      const rv3 = Reflect.apply(returner, proxy, expectedArgs.slice(0));
+      expect(rv3).toBe(rv);
     });
 
     it("on the construct trap", function() {
       const base = function() {};
       base.prototype.validate = () => true;
+
       shadow = function(arg1) { this.arg = arg1; };
+
+      revoke();
+      let obj = Proxy.revocable(shadow, handler);
+      proxy = obj.proxy;
+      revoke = obj.revoke;
+
       const rv1 = Reflect.construct(shadow, [ "foo" ], base);
       expect(rv1.arg).toBe("foo");
       expect(rv1.validate()).toBe(true);
 
-      const rv2 = handler.construct(shadow, [ "foo" ], base);
-      expect(rv2.arg).toBe("foo");
+      const rv2 = handler.construct(shadow, [ "bar" ], base);
+      expect(rv2.arg).toBe("bar");
       expect(rv2.validate()).toBe(true);
+
+      const rv3 = Reflect.construct(proxy, [ "foo" ], base);
+      expect(rv3.arg).toBe("foo");
+      expect(rv3.validate()).toBe(true);
     });
   });
 });
