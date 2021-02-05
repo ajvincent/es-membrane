a96663bebf6c358ee0a829b9252b00ca7908a45b
Alexander J. Vincent <ajvincent@gmail.com>
2021-02-04 21:18:34 -0800

    Use headless browsers for GUI tests.

--- gui-tests.conf.js
+++ gui-tests.conf.js
@@ -55,7 +55,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome'],
+    browsers: ['FirefoxHeadless', 'ChromeHeadless'],
 
 
     // Continuous Integration mode
