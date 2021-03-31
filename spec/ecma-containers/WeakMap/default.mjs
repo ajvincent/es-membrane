14e76703842ebdb9973b842ab9097dd7002130f5
Alexander J. Vincent <ajvincent@gmail.com>
2021-03-30 22:44:16 -0700

    spec/ecma-containers/WeakMap/default.mjs

--- spec/ecma-containers/WeakMap/default.mjs
+++ spec/ecma-containers/WeakMap/default.mjs
@@ -1,32 +1,17 @@
-/*
-import "../docs/dist/es6-modules/Membrane.js";
-import "../docs/dist/es6-modules/MembraneMocks.js";
-*/
-if (typeof Membrane != "function") {
-  if (typeof require == "function") {
-    var { Membrane } = require("../../../docs/dist/node/es-membrane.js");
-  }
-  else
-    throw new Error("Unable to run tests: cannot get Membrane");
-}
+import Membrane from "../../../source/core/Membrane.mjs";
 
 it(
   "WeakMap instances by default in a membrane work like they do without a membrane",
   function() {
-    "use strict";
-  
-    let membrane, wetHandler, dryHandler, dampHandler, wetMap, dryMap, dampMap;
+    let membrane, wetHandler, dryHandler, wetMap, dryMap;
     {
       const MUSTCREATE = Object.freeze({ mustCreate: true });
       membrane = new Membrane();
       wetHandler  = membrane.getGraphByName("wet", MUSTCREATE);
       dryHandler  = membrane.getGraphByName("dry", MUSTCREATE);
-      dampHandler = membrane.getGraphByName("damp", MUSTCREATE);
-  
+
       wetMap  = new WeakMap();
       dryMap  = membrane.convertArgumentToProxy(wetHandler, dryHandler,  wetMap);
-      // we rarely create proxies this way in our tests, so this'll be useful
-      dampMap = membrane.convertArgumentToProxy(dryHandler, dampHandler, dryMap);
     }
   
     function checkMap(map, keys, values, shouldHave = true) {
