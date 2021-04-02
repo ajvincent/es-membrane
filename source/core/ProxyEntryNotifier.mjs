67c7cb6fbb8841ce1fad140a172622dd779078e0
Alexander J. Vincent <ajvincent@gmail.com>
2021-04-01 22:38:34 -0700

    source/core/ProxyEntryNotifier.mjs

---
+++ source/core/ProxyEntryNotifier.mjs
@@ -0,0 +1,191 @@
+/** @module source/core/ProxyEntryNotifier.mjs */
+
+/**
+ * @fileoverview
+ *
+ * The ProxyEntryNotifier represents the best way to notify an observer that a proxy is
+ * about to enter an object graph.  It's for uninitialized proxies entering an object graph
+ * through a known object or proxy, so that we can apply distortions to the uninitialized
+ * proxy.
+ *
+ * The best example I can give is an observer proxy receiving a raw object.  The only chance
+ * to attach distortions to the proxy for the raw object is before the real observer receives
+ * the object's proxy.
+ */
+
+import {
+  allTraps,
+  defineNWNCProperties,
+} from "./utilities/shared.mjs";
+
+const trapsAsSet = new Set(allTraps);
+
+/**
+ * The message class.
+ *
+ * @public
+ */
+class ProxyEntryMessage {
+  /**
+   * @param {Proxy | Object}   entryTarget An object about to receive the proxyTarget as a property or argument.
+   * @param {string}           trapName    The name of the trap.
+   * @param {number | string?} entryPoint  A property name, or an argument index for a function call.  Null if not applicable.
+   * @param {Proxy | Object}   proxyTarget The proxy target to use for the modify rules API, or a DistortionsListener.
+   */
+  constructor(entryTarget, trapName, entryPoint, proxyTarget) {
+    defineNWNCProperties(this, {
+      /** @public, @readonly */
+      entryTarget,
+
+      /** @public, @readonly */
+      trapName,
+
+      /** @public, @readonly */
+      entryPoint,
+
+      /** @public, @readonly */
+      proxyTarget
+    }, true);
+
+    Object.freeze(this);
+  }
+}
+Object.freeze(ProxyEntryMessage);
+Object.freeze(ProxyEntryMessage.prototype);
+
+/**
+ * @typedef {WeakMap<ObjectGraph, WeakMap<entryTarget, Function>>} GraphEntryMap;
+ */
+
+/**
+ * Our entry notifier class.
+ *
+ * @public
+ */
+export default class ProxyEntryNotifier {
+  /**
+   * @param {ProxyCylinderMap} __cylinderMap__ The cylinder map for the membrane.
+   */
+  constructor(__cylinderMap__) {
+    defineNWNCProperties(this, {
+      /** @private */
+      __cylinderMap__,
+
+      /**
+       * @type {Map<string, GraphEntryMap>}
+       * @private
+       */
+      __rootMap__: new Map(),
+    }, false);
+
+    Object.freeze(this);
+  }
+
+  /**
+   * Set an entry observer.
+   *
+   * @param {ObjectGraph}                 objectGraph The object graph owning the entry target.
+   * @param {string}                      trapName    The trap name to apply.
+   * @param {Proxy | Object}              entryTarget An object which will receive proxy targets as a property or argument.
+   * @param {Function<ProxyEntryMessage>} observer    The observer to notify.
+   *
+   * @public
+   */
+  setEntryObserver(objectGraph, trapName, entryTarget, observer) {
+    if (!trapsAsSet.has(trapName)) {
+      throw new Error(`Unknown trap "${trapName}"!`);
+    }
+
+    if (typeof observer !== "function")
+      throw new Error("observer is not a function!");
+
+    {
+      const membrane = objectGraph.membrane;
+      const match = membrane.getMembraneProxy(objectGraph.graphName, entryTarget)[1];
+      if (match !== entryTarget)
+        throw new Error("Entry target does not belong to object graph!");
+    }
+
+    if (!this.__rootMap__.has(trapName)) {
+      this.__rootMap__.set(trapName, new WeakMap);
+    }
+
+    const graphMap = this.__rootMap__.get(trapName);
+    if (!graphMap.has(objectGraph)) {
+      graphMap.set(objectGraph, new WeakMap);
+    }
+
+    const entryMap = graphMap.get(objectGraph);
+    if (entryMap.has(entryTarget))
+      throw new Error("Entry trap has already been set for this object graph, trap name and entry target!");
+
+    entryMap.set(entryTarget, observer);
+  }
+
+  /**
+   * @param {ObjectGraph}      sourceGraph The object graph of the entry object.
+   * @param {ObjectGraph}      targetGraph The object graph of the proxy.
+   * @param {Proxy | Object}   entryObject An object about to receive the proxyTarget as a property or argument.
+   * @param {string}           trapName    The name of the trap.
+   * @param {number | string?} entryPoint  A property name, or an argument index for a function call.  Null if not applicable.
+   * @param {Proxy}            proxy       The proxy to use for the modify rules API, or a DistortionsListener.
+   *
+   * @package
+   */
+  notify(sourceGraph, targetGraph, entryObject, trapName, entryPoint, proxy) {
+    const graphMap = this.__rootMap__.get(trapName);
+    if (!graphMap)
+      return;
+
+    const entryCylinder = this.__cylinderMap__.get(entryObject);
+    if (!entryCylinder)
+      return;
+
+    const proxyCylinder = this.__cylinderMap__.get(proxy);
+    if (!proxyCylinder)
+      return;
+
+    this.__notifyGraph__(graphMap, sourceGraph, entryCylinder, trapName, entryPoint, proxyCylinder);
+    this.__notifyGraph__(graphMap, targetGraph, entryCylinder, trapName, entryPoint, proxyCylinder);
+  }
+
+  /**
+   * Notify an observer belonging to a specific object graph and entry target.
+   * @param {GraphEntryMap}    graphMap      A shared weak map for the specific trap.
+   * @param {ObjectGraph}      graph         An object graph we register the observer for.
+   * @param {ProxyCylinder}    entryCylinder The cylinder owning the entry target.
+   * @param {string}           trapName      The name of the trap.
+   * @param {number | string?} entryPoint    A property name, or an argument index for a function call.  Null if not applicable.
+   * @param {ProxyCylinder}    proxyCylinder The cylinder owning the proxy target we want to observe.
+   *
+   * @private
+   */
+  __notifyGraph__(graphMap, graph, entryCylinder, trapName, entryPoint, proxyCylinder) {
+    const entryMap = graphMap.get(graph);
+    if (!entryMap)
+      return;
+
+    const entryTarget = entryCylinder.getProxy(graph.graphName);
+    const observer = entryMap.get(entryTarget);
+
+    if (!observer)
+      return;
+
+    const message = new ProxyEntryMessage(
+      entryTarget,
+      trapName,
+      entryPoint,
+      proxyCylinder.getProxy(graph.graphName)
+    );
+
+    try {
+      observer(message);
+    }
+    catch (ex) {
+      // do nothing
+    }
+  }
+}
+
+Object.freeze(ProxyEntryNotifier);
+Object.freeze(ProxyEntryNotifier.prototype);
---
+++ spec/core/ProxyEntryNotifier.mjs
@@ -0,0 +1,336 @@
+import ProxyEntryNotifier from "../../source/core/ProxyEntryNotifier.mjs";
+
+import {
+  expectValueDescriptor,
+} from "../helpers/expectDataDescriptor.mjs";
+
+describe("ProxyEntryNotifier", () => {
+  let cylinderMap, notifier;
+  beforeEach(() => {
+    cylinderMap = new WeakMap;
+    notifier = new ProxyEntryNotifier(cylinderMap);
+  });
+
+  afterEach(() => {
+    cylinderMap = null;
+    notifier = null;
+  });
+
+  it("class is frozen", () => {
+    expect(Object.isFrozen(ProxyEntryNotifier)).toBe(true);
+    expect(Object.isFrozen(ProxyEntryNotifier.prototype)).toBe(true);
+  });
+
+  it("instance is frozen", () => {
+    expect(Object.isFrozen(notifier)).toBe(true);
+  });
+
+  describe("setEntryObserver()", () => {
+    const objectGraph = {
+      membrane: {
+        getMembraneProxy: jasmine.createSpy("getMembraneProxy"),
+      },
+      graphName: "wet",
+    };
+
+    const observer = jasmine.createSpy("observer");
+
+    const entryTarget = {};
+
+    beforeEach(() => {
+      objectGraph.membrane.getMembraneProxy.and.returnValue([null, entryTarget]);
+      objectGraph.membrane.getMembraneProxy.calls.reset();
+
+      observer.and.stub();
+      observer.calls.reset();
+    });
+
+    describe("stores an observer for", () => {
+      it("the tuple of object graph, trap name and entry target", () => {
+        expect(() => {
+          notifier.setEntryObserver(objectGraph, "apply", entryTarget, observer);
+        }).not.toThrow();
+
+        expect(objectGraph.membrane.getMembraneProxy).toHaveBeenCalledOnceWith("wet", entryTarget);
+        expect(observer).toHaveBeenCalledTimes(0);
+      });
+
+      it("a different object graph", () => {
+        notifier.setEntryObserver(objectGraph, "apply", entryTarget, observer);
+
+        const dryGraph = {
+          membrane: {
+            getMembraneProxy: jasmine.createSpy("getMembraneProxy"),
+          },
+          graphName: "dry",
+        };
+        const dryEntryTarget = {};
+        dryGraph.membrane.getMembraneProxy.and.returnValue([null, dryEntryTarget]);
+
+        expect(() => {
+          notifier.setEntryObserver(dryGraph, "apply", dryEntryTarget, observer);
+        }).not.toThrow();
+
+        expect(objectGraph.membrane.getMembraneProxy).toHaveBeenCalledOnceWith("wet", entryTarget);
+        expect(dryGraph.membrane.getMembraneProxy).toHaveBeenCalledOnceWith("dry", dryEntryTarget);
+        expect(observer).toHaveBeenCalledTimes(0);
+      });
+
+      it("a different trap", () => {
+        notifier.setEntryObserver(objectGraph, "apply", entryTarget, observer);
+
+        objectGraph.membrane.getMembraneProxy.calls.reset();
+
+        expect(() => {
+          notifier.setEntryObserver(objectGraph, "defineProperty", entryTarget, observer);
+        }).not.toThrow();
+
+        expect(objectGraph.membrane.getMembraneProxy).toHaveBeenCalledOnceWith("wet", entryTarget);
+        expect(observer).toHaveBeenCalledTimes(0);
+      });
+
+      it("a different entry target", () => {
+        const firstTarget = {};
+        objectGraph.membrane.getMembraneProxy.and.returnValue([null, firstTarget]);
+        notifier.setEntryObserver(objectGraph, "apply", firstTarget, observer);
+
+        objectGraph.membrane.getMembraneProxy.calls.reset();
+        objectGraph.membrane.getMembraneProxy.and.returnValue([null, entryTarget]);
+
+        expect(() => {
+          notifier.setEntryObserver(objectGraph, "apply", entryTarget, observer);
+        }).not.toThrow();
+
+        expect(objectGraph.membrane.getMembraneProxy).toHaveBeenCalledOnceWith("wet", entryTarget);
+        expect(observer).toHaveBeenCalledTimes(0);
+      });
+    });
+
+    describe("throws for", () => {
+      it("an unknown trap", () => {
+        expect(() => {
+          notifier.setEntryObserver(objectGraph, "foo", entryTarget, observer)
+        }).toThrowError(`Unknown trap "foo"!`);
+
+        expect(objectGraph.membrane.getMembraneProxy).toHaveBeenCalledTimes(0);
+        expect(observer).toHaveBeenCalledTimes(0);
+      });
+
+      it("a non-function observer", () => {
+        expect(() => {
+          notifier.setEntryObserver(objectGraph, "apply", entryTarget, {})
+        }).toThrowError("observer is not a function!");
+
+        expect(objectGraph.membrane.getMembraneProxy).toHaveBeenCalledTimes(0);
+        expect(observer).toHaveBeenCalledTimes(0);
+      });
+
+      it("a mismatched membrane proxy", () => {
+        objectGraph.membrane.getMembraneProxy.and.returnValue([null, {}]);
+        expect(() => {
+          notifier.setEntryObserver(objectGraph, "apply", entryTarget, observer)
+        }).toThrowError("Entry target does not belong to object graph!");
+  
+        expect(objectGraph.membrane.getMembraneProxy).toHaveBeenCalledOnceWith("wet", entryTarget);
+        expect(observer).toHaveBeenCalledTimes(0);
+      });
+  
+      it("setting an observer twice for the same object graph, trap name and entry target", () => {
+        notifier.setEntryObserver(objectGraph, "apply", entryTarget, observer);
+  
+        expect(() => {
+          notifier.setEntryObserver(objectGraph, "apply", entryTarget, observer)
+        }).toThrowError("Entry trap has already been set for this object graph, trap name and entry target!");
+  
+        expect(() => {
+          notifier.setEntryObserver(objectGraph, "apply", entryTarget, () => {})
+        }).toThrowError("Entry trap has already been set for this object graph, trap name and entry target!");
+      });
+    });
+  });
+
+  describe("notify()", () => {
+    const wetGraph = {
+      membrane: {
+        getMembraneProxy: jasmine.createSpy("getMembraneProxy"),
+      },
+      graphName: "wet",
+    };
+    const wetObserver = jasmine.createSpy("observer");
+    const wetEntryTarget = {};
+    const wetProxy = {};
+
+    const dryGraph = {
+      membrane: {
+        getMembraneProxy: jasmine.createSpy("getMembraneProxy"),
+      },
+      graphName: "dry",
+    };
+    const dryObserver = jasmine.createSpy("observer");
+    const dryEntryTarget = {};
+    const dryProxy = {};
+
+    const entryCylinder = jasmine.createSpyObj("entryCylinder", ["getProxy"]);
+
+    const proxyCylinder = jasmine.createSpyObj("dryCylinder", ["getProxy"]);
+
+    function setWetObserver() {
+      notifier.setEntryObserver(wetGraph, "apply", wetEntryTarget, wetObserver);
+      entryCylinder.getProxy.and.returnValue(wetEntryTarget);
+      proxyCylinder.getProxy.and.returnValue(wetProxy);
+    }
+
+    function sendNotify() {
+      notifier.notify(wetGraph, dryGraph, dryEntryTarget, "apply", 12, dryProxy);
+    }
+
+    function expectMessage(message, entryTarget, trapName, proxyTarget) {
+      expect(Reflect.ownKeys(message)).toEqual([
+        "entryTarget",
+        "trapName",
+        "entryPoint",
+        "proxyTarget"
+      ]);
+
+      expectValueDescriptor(
+        entryTarget, false, true, false, Reflect.getOwnPropertyDescriptor(message, "entryTarget")
+      );
+      expectValueDescriptor(
+        trapName, false, true, false, Reflect.getOwnPropertyDescriptor(message, "trapName")
+      );
+      expectValueDescriptor(
+        12, false, true, false, Reflect.getOwnPropertyDescriptor(message, "entryPoint")
+      );
+      expectValueDescriptor(
+        proxyTarget, false, true, false, Reflect.getOwnPropertyDescriptor(message, "proxyTarget")
+      );
+
+      expect(Object.isFrozen(message)).toBe(true);
+    }
+
+    beforeEach(() => {
+      wetGraph.membrane.getMembraneProxy.and.returnValue([null, wetEntryTarget]);
+      wetGraph.membrane.getMembraneProxy.calls.reset();
+
+      dryGraph.membrane.getMembraneProxy.and.returnValue([null, dryEntryTarget]);
+      dryGraph.membrane.getMembraneProxy.calls.reset();
+
+      wetObserver.and.stub();
+      wetObserver.calls.reset();
+
+      dryObserver.and.stub();
+      dryObserver.calls.reset();
+
+      cylinderMap.set(dryEntryTarget, entryCylinder);
+      cylinderMap.set(dryProxy,       proxyCylinder);
+
+      entryCylinder.getProxy.calls.reset();
+      proxyCylinder.getProxy.calls.reset();
+    });
+
+    describe("with the wet observer installed", () => {
+      beforeEach(setWetObserver);
+
+      it("notifies the wet observer for a matching source graph, entry object, and trap name", () => {
+        sendNotify();
+
+        expect(entryCylinder.getProxy).toHaveBeenCalledOnceWith("wet");
+        expect(proxyCylinder.getProxy).toHaveBeenCalledOnceWith("wet");
+
+        expect(wetObserver).toHaveBeenCalledTimes(1);
+        const args = wetObserver.calls.first().args;
+        expect(args.length).toBe(1);
+
+        const message = args[0];
+        expectMessage(message, wetEntryTarget, "apply", wetProxy);
+      });
+
+      it("does not call the observer with a different source graph", () => {
+        notifier.notify({}, dryGraph, dryEntryTarget, "apply", 12, dryProxy);
+        expect(entryCylinder.getProxy).toHaveBeenCalledTimes(0);
+        expect(wetObserver).toHaveBeenCalledTimes(0);
+      });
+
+      it("does not call the observer with a different entry target", () => {
+        notifier.notify(wetGraph, dryGraph, {}, "apply", 12, dryProxy);
+        expect(entryCylinder.getProxy).toHaveBeenCalledTimes(0);
+        expect(wetObserver).toHaveBeenCalledTimes(0);
+      });
+
+      it("does not call the observer with a different trap name", () => {
+        notifier.notify(wetGraph, dryGraph, dryEntryTarget, "get", 12, dryProxy);
+        expect(entryCylinder.getProxy).toHaveBeenCalledTimes(0);
+        expect(wetObserver).toHaveBeenCalledTimes(0);
+      });
+
+      it("does not propagate an exception thrown from an observer", () => {
+        wetObserver.and.throwError(new Error("whoops"));
+        expect(sendNotify).not.toThrow();
+
+        expect(entryCylinder.getProxy).toHaveBeenCalledOnceWith("wet");
+        expect(proxyCylinder.getProxy).toHaveBeenCalledOnceWith("wet");
+
+        expect(wetObserver).toHaveBeenCalledTimes(1);
+        const args = wetObserver.calls.first().args;
+        expect(args.length).toBe(1);
+
+        const message = args[0];
+        expectMessage(message, wetEntryTarget, "apply", wetProxy);
+      });
+    });
+
+    it("with the dry observer installed", () => {
+      notifier.setEntryObserver(dryGraph, "apply", dryEntryTarget, dryObserver);
+      entryCylinder.getProxy.and.returnValue(dryEntryTarget);
+      proxyCylinder.getProxy.and.returnValue(dryProxy);
+
+      sendNotify();
+
+      expect(entryCylinder.getProxy).toHaveBeenCalledOnceWith("dry");
+      expect(proxyCylinder.getProxy).toHaveBeenCalledOnceWith("dry");
+
+      expect(dryObserver).toHaveBeenCalledTimes(1);
+      const args = dryObserver.calls.first().args;
+      expect(args.length).toBe(1);
+
+      const message = args[0];
+      expectMessage(message, dryEntryTarget, "apply", dryProxy);
+    });
+
+    it("with both the wet and dry observers installed", () => {
+      setWetObserver();
+      notifier.setEntryObserver(dryGraph, "apply", dryEntryTarget, dryObserver);
+      entryCylinder.getProxy.and.returnValues(wetEntryTarget, dryEntryTarget);
+      proxyCylinder.getProxy.and.returnValues(wetProxy, dryProxy);
+
+      // testing that an exception thrown in one graph doesn't stop the other graph from running
+      wetObserver.and.throwError(new Error("whoops"));
+
+      expect(sendNotify).not.toThrow();
+
+      expect(entryCylinder.getProxy).toHaveBeenCalledTimes(2);
+      expect(entryCylinder.getProxy).toHaveBeenCalledWith("wet");
+      expect(entryCylinder.getProxy).toHaveBeenCalledWith("dry");
+
+      expect(proxyCylinder.getProxy).toHaveBeenCalledTimes(2);
+      expect(proxyCylinder.getProxy).toHaveBeenCalledWith("wet");
+      expect(proxyCylinder.getProxy).toHaveBeenCalledWith("dry");
+
+      expect(wetObserver).toHaveBeenCalledTimes(1);
+      const wetArgs = wetObserver.calls.first().args;
+      expect(wetArgs.length).toBe(1);
+
+      const wetMessage = wetArgs[0];
+      expectMessage(wetMessage, wetEntryTarget, "apply", wetProxy);
+
+      expect(dryObserver).toHaveBeenCalledTimes(1);
+      const dryArgs = dryObserver.calls.first().args;
+      expect(dryArgs.length).toBe(1);
+
+      const dryMessage = dryArgs[0];
+      expectMessage(dryMessage, dryEntryTarget, "apply", dryProxy);
+
+      expect(wetMessage === dryMessage).toBe(false);
+    });
+  });
+});
