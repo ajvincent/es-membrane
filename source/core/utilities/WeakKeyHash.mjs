c61c5ffcd920028d608acdf068a03f7e8d80004e
Alexander J. Vincent <ajvincent@gmail.com>
2021-04-03 23:35:34 -0700

    source/core/utilities/WeakKeyHash.mjs, with specs.

---
+++ source/core/utilities/WeakKeyHash.mjs
@@ -0,0 +1,157 @@
+/**
+ * @module source/core/utilities/WeakKeyHash.mjs
+ *
+ * @fileoverview
+ * This is really a diagnostic tool, emulating the CompositeKey concept.
+ * The goal is to use this to transform weak tuples into easily identifable
+ * string hashes and WeakCompositeKey objects.
+ *
+ * @see https://github.com/tc39/proposal-richer-keys/tree/master/compositeKey
+ */
+
+import {
+  defineNWNCProperties,
+} from "./shared.mjs";
+
+/**
+ * @package
+ */
+class WeakCompositeKey extends WeakSet {
+  constructor(keys, hash) {
+    super(keys);
+
+    defineNWNCProperties(this, { hash }, true);
+    Object.freeze(this);
+  }
+
+  /** @override */
+  add() {
+    return false;
+  }
+
+  /** @override */
+  delete() {
+    return false;
+  }
+}
+Object.freeze(WeakCompositeKey);
+Object.freeze(WeakCompositeKey.prototype);
+
+/**
+ * @package
+ */
+export default class WeakKeyHash {
+  /**
+   * @param {string[]} __keys__ The keys to require.
+   */
+  constructor(...__keys__) {
+    /**
+     * @private
+     */
+    this.__hashCount__ = 0;
+
+    __keys__.sort();
+
+    if ((__keys__.length < 2) || __keys__.some(key => typeof key !== "string")) {
+      throw new Error("weakDictionary must have at least two string keys, and no symbol keys!");
+    }
+
+    defineNWNCProperties(this, {
+      /**
+       * @type {WeakMap<object, integer>}
+       * @private
+       */
+      __hashToIndex__: new WeakMap,
+
+      /**
+       * @type {WeakMap<object, WeakMap<object, WeakMap | WeakKey>>}
+       * @private
+       */
+      __root__: new WeakMap(),
+
+      __keySet__: new Set(__keys__),
+    }, false);
+
+    if (__keys__.length !== this.__keySet__.size)
+      throw new Error("duplicate keys found!");
+
+    Reflect.preventExtensions(this);
+  }
+
+  /**
+   * Build a hash object from a dictionary of keys.
+   *
+   * @param {Object<key, Object>} weakDictionary A dictionary of values to hold weakly.
+   *
+   * @returns {Object} Unique for the dictionary passed in.
+   * @private
+   */
+  __buildHash__(weakDictionary) {
+    const keys = Reflect.ownKeys(weakDictionary);
+    keys.sort();
+    if (keys.length !== this.__keySet__.size) {
+      throw new Error("Wrong number of keys!");
+    }
+    keys.forEach(key => {
+      if (!this.__keySet__.has(key)) {
+        throw new Error("Unknown key: " + key);
+      }
+    });
+
+    let hash = {};
+    for (let key of keys) {
+      const value = weakDictionary[key];
+      if (!this.__hashToIndex__.has(value))
+        this.__hashToIndex__.set(value, ++this.__hashCount__);
+      hash[key] = this.__hashToIndex__.get(value);
+    }
+
+    return hash;
+  }
+
+  /**
+   * Build a human-readable hash from a dictionary of keys.
+   *
+   * @param {Object<key, Object>} weakDictionary A dictionary of values to hold weakly.
+   *
+   * @returns {string} Unique for the dictionary passed in.
+   * @public
+   */
+  buildHash(weakDictionary) {
+    return JSON.stringify(this.__buildHash__(weakDictionary));
+  }
+
+  /**
+   *
+   * @param {Object<key, Object>} weakDictionary A dictionary of values to hold weakly.
+   * @param {boolean}             buildIfMissing True if we should build a new step for each key.
+   * @returns {WeakCompositeKey?}
+   */
+  getKey(weakDictionary, buildIfMissing = false) {
+    const hash = this.__buildHash__(weakDictionary);
+
+    // Beyond this point, we cannot throw.
+    const keys = Reflect.ownKeys(hash);
+    const values = [];
+
+    let current = this.__root__, key = keys.shift();
+    while (key && current) {
+      const value = weakDictionary[key];
+      values.push(value);
+
+      if (!current.has(value) && buildIfMissing) {
+        current.set(
+          value,
+          keys.length > 0 ? new WeakMap : new WeakCompositeKey(values, JSON.stringify(hash))
+        );
+      }
+
+      current = current.get(value);
+      key = keys.shift();
+    }
+
+    return current;
+  }
+}
+Object.freeze(WeakKeyHash);
+Object.freeze(WeakKeyHash.prototype);
---
+++ spec/core/utilities/WeakKeyHash.mjs
@@ -0,0 +1,276 @@
+import WeakKeyHash from "../../../source/core/utilities/WeakKeyHash.mjs";
+
+const value0 = {}, value1 = {}, value2 = {}, value3 = {};
+
+describe("WeakKeyHash", () => {
+  let hasher;
+  afterEach(() => hasher = null);
+
+  describe("constructor", () => {
+
+    it("accepts any number of string arguments", () => {
+      const args = [];
+      for (let i = 0; i < 10; i++)
+        args.push("part_0" + i);
+      for (let i = 10; i < 20; i++)
+        args.push("part_" + i);
+      expect(() => {
+        hasher = new WeakKeyHash(...args);
+      }).not.toThrow();
+    });
+
+    it("throws if the number of keys at initialization is too low", () => {
+      expect(() => {
+        hasher = new WeakKeyHash();
+      }).toThrowError("weakDictionary must have at least two string keys, and no symbol keys!");
+
+      expect(() => {
+        hasher = new WeakKeyHash("part1");
+      }).toThrowError("weakDictionary must have at least two string keys, and no symbol keys!");
+    });
+
+    it("throws if there is a symbol key in the sequence", () => {
+      expect(() => {
+        hasher = new WeakKeyHash(Symbol("part1"), "part2", "part3");
+      }).toThrow();
+    });
+
+    it("throws if there is a non-primitive key in the sequence", () => {
+      expect(() => {
+        hasher = new WeakKeyHash("part2", {}, "part3");
+      }).toThrowError("weakDictionary must have at least two string keys, and no symbol keys!");
+    });
+
+    it("throws if there is a duplicate key in the sequence", () => {
+      expect(() => {
+        hasher = new WeakKeyHash("part2", "part2", "part3");
+      }).toThrowError("duplicate keys found!");
+    });
+
+    it("is not extensible", () => {
+      hasher = new WeakKeyHash("part1", "part2");
+      expect(Reflect.isExtensible(hasher)).toBe(false);
+    });
+  });
+
+  describe(".buildHash()", () => {
+    beforeEach(() => hasher = new WeakKeyHash("part1", "part2"));
+    it("builds a hash for each unique dictionary", () => {
+      const key1 = {
+        part1: value0,
+        part2: value1,
+      };
+
+      const hash1 = hasher.buildHash(key1);
+      expect(typeof hash1).toBe("string");
+      expect(hasher.buildHash(key1)).toBe(hash1);
+      expect(hasher.buildHash({
+        part1: value0,
+        part2: value1,
+      })).toBe(hash1);
+
+      expect(hasher.buildHash({
+        part2: value1,
+        part1: value0,
+      })).toBe(hash1);
+
+      const key2 = {
+        part1: value0,
+        part2: value2,
+      };
+
+      const hash2 = hasher.buildHash(key2);
+      expect(typeof hash2).toBe("string");
+      expect(hash2).not.toBe(hash1);
+      expect(hasher.buildHash(key2)).toBe(hash2);
+      expect(hasher.buildHash({
+        part1: value0,
+        part2: value2,
+      })).toBe(hash2);
+
+      expect(hasher.buildHash({
+        part1: value0,
+        part2: value1,
+      })).toBe(hash1);
+
+      const key3 = {
+        part1: value3,
+        part2: value1,
+      };
+
+      const hash3 = hasher.buildHash(key3);
+      expect(typeof hash3).toBe("string");
+      expect(hash3).not.toBe(hash2);
+      expect(hash3).not.toBe(hash1);
+      expect(hasher.buildHash(key3)).toBe(hash3);
+      expect(hasher.buildHash({
+        part1: value3,
+        part2: value1,
+      })).toBe(hash3);
+
+      expect(hasher.buildHash({
+        part1: value0,
+        part2: value2,
+      })).toBe(hash2);
+
+      expect(hasher.buildHash({
+        part1: value0,
+        part2: value1,
+      })).toBe(hash1);
+    });
+
+    it("throws if any value in the key is a primitive", () => {
+      expect(() => {
+        hasher.buildHash({
+          part1: value0,
+          part2: Symbol("foo"),
+        });
+      }).toThrow();
+    });
+
+    it("throws for an unknown key", () => {
+      expect(() => {
+        hasher.buildHash({
+          part1: value0,
+          part3: value1,
+        });
+      }).toThrowError("Unknown key: part3");
+    });
+
+    it("throws for a missing key", () => {
+      expect(() => {
+        hasher.buildHash({
+          part1: value0,
+        });
+      }).toThrowError("Wrong number of keys!");
+    });
+
+    it("throws for an extra key", () => {
+      expect(() => {
+        hasher.buildHash({
+          part1: value0,
+          part2: value1,
+          part3: value2,
+        });
+      }).toThrowError("Wrong number of keys!");
+    });
+  });
+
+  describe(".getKey()", () => {
+    beforeEach(() => hasher = new WeakKeyHash("part1", "part2"));
+    it("returns undefined for an unknown key when the second argument is not passed in as true", () => {
+      expect(hasher.getKey({
+        part1: value0,
+        part2: value1,
+      })).toBe(undefined);
+    });
+
+    it("builds a hash for each unique dictionary", () => {
+      hasher = new WeakKeyHash("part1", "part2");
+      const key1 = {
+        part1: value0,
+        part2: value1,
+      };
+
+      const composite1 = hasher.getKey(key1, true);
+      expect(composite1 instanceof WeakSet).toBe(true);
+      expect(composite1.hash).toBe(hasher.buildHash(key1));
+      expect(hasher.getKey(key1)).toBe(composite1);
+      expect(hasher.getKey({
+        part1: value0,
+        part2: value1,
+      })).toBe(composite1);
+
+      const key2 = {
+        part1: value0,
+        part2: value2,
+      };
+
+      const composite2 = hasher.getKey(key2, true);
+      expect(composite2 instanceof WeakSet).toBe(true);
+      expect(composite2.hash).toBe(hasher.buildHash(key2));
+      expect(composite2 === composite1).toBe(false);
+
+      const key3 = {
+        part1: value3,
+        part2: value1,
+      };
+
+      const hash3 = hasher.buildHash(key3);
+      expect(hasher.getKey(key3)).toBe(undefined);
+
+      const composite3 = hasher.getKey(key3, true);
+      expect(composite3 instanceof WeakSet).toBe(true);
+      expect(composite3.hash).toBe(hash3);
+      expect(hasher.getKey(key3)).toBe(composite3);
+    });
+
+    it("throws if any value in the key is a primitive", () => {
+      hasher = new WeakKeyHash("part1", "part2");
+      expect(() => {
+        hasher.getKey({
+          part1: value0,
+          part2: Symbol("foo"),
+        });
+      }).toThrow();
+
+      expect(() => {
+        hasher.getKey({
+          part1: value0,
+          part2: Symbol("foo"),
+        }, true);
+      }).toThrow();
+    });
+
+    it("throws for an unknown key", () => {
+      hasher = new WeakKeyHash("part1", "part2");
+      expect(() => {
+        hasher.getKey({
+          part1: value0,
+          part3: value1,
+        });
+      }).toThrowError("Unknown key: part3");
+
+      expect(() => {
+        hasher.getKey({
+          part1: value0,
+          part3: value1,
+        }, true);
+      }).toThrowError("Unknown key: part3");
+    });
+
+    it("throws for a missing key", () => {
+      hasher = new WeakKeyHash("part1", "part2");
+      expect(() => {
+        hasher.getKey({
+          part1: value0,
+        });
+      }).toThrowError("Wrong number of keys!");
+
+      expect(() => {
+        hasher.getKey({
+          part1: value0,
+        }, true);
+      },).toThrowError("Wrong number of keys!");
+    });
+
+    it("throws for an extra key", () => {
+      hasher = new WeakKeyHash("part1", "part2");
+      expect(() => {
+        hasher.getKey({
+          part1: value0,
+          part2: value1,
+          part3: value2,
+        });
+      }).toThrowError("Wrong number of keys!");
+
+      expect(() => {
+        hasher.getKey({
+          part1: value0,
+          part2: value1,
+          part3: value2,
+        }, true);
+      }).toThrowError("Wrong number of keys!");
+    });
+  });
+});
