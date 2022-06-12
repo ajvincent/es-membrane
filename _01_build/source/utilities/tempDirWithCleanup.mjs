a80476c40714b5eebb9b09546c06b81a08cd58fa
Alexander J. Vincent <ajvincent@gmail.com>
2022-06-11 18:48:27 -0700

    Add a missed build module.

---
+++ _01_build/source/utilities/tempDirWithCleanup.mjs
@@ -0,0 +1,32 @@
+import fs from "fs/promises";
+import path from "path";
+import os from "os";
+import { Deferred } from "./PromiseTypes.mjs";
+export class TemporaryDirWithPromise {
+    tempDir = "";
+    resolve;
+    promise;
+    constructor() {
+        const { resolve, promise } = new Deferred;
+        this.resolve = resolve;
+        this.promise = promise;
+    }
+}
+void (TemporaryDirWithPromise);
+/**
+ * Create a temporary directory with a promise to clean it up later.
+ *
+ * @returns The directory and promise.
+ */
+export default async function tempDirWithCleanup() {
+    const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), "es-membrane-"));
+    const d = new Deferred;
+    let { promise } = d;
+    promise = promise.then(() => fs.rm(tempDir, { recursive: true }));
+    return {
+        tempDir,
+        resolve: d.resolve,
+        promise,
+    };
+}
+//# sourceMappingURL=tempDirWithCleanup.mjs.map
