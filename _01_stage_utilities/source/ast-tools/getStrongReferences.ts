8f5bf79587e55c2eccb66b4648ff630cb25dbaed
Alexander J. Vincent <ajvincent@gmail.com>
2024-07-21 17:54:19 -0700

    Use a dictionary of arguments.  Still in progress.

--- _01_stage_utilities/source/ast-tools/getStrongReferences.ts
+++ _01_stage_utilities/source/ast-tools/getStrongReferences.ts
@@ -26,15 +26,23 @@ interface StrongReference {
   pathsToObjects: readonly string[];
 }
 
+export interface ParameterLocation {
+  pathToTypeScriptFile: string;
+  className: string;
+  methodName: string;
+  paramName: string;
+}
+
 export default
 async function holdsArgument(
-  pathToTypeScriptFile: string,
-  className: string,
-  methodName: string,
-  paramName: string,
+  argumentLocation: ParameterLocation
 ): Promise<readonly StrongReference[]>
 {
-  pathToTypeScriptFile = path.resolve(projectDir, pathToTypeScriptFile);
+  const {
+    className, methodName, paramName
+  } = argumentLocation;
+
+  const pathToTypeScriptFile = path.resolve(projectDir, argumentLocation.pathToTypeScriptFile);
   const tsSource = await fs.readFile(pathToTypeScriptFile, { encoding: "utf-8" });
 
   const {
@@ -72,12 +80,17 @@ async function holdsArgument(
     throw new Error("how did we not have a scope?");
 
   debugger;
-  const paramAsVariable = methodScope.variables.find(v => v.name === (param as TSESTree.Identifier).name);
+  const paramAsVariable: Variable | undefined = methodScope.variables.find(v => v.name === (param as TSESTree.Identifier).name);
   if (!paramAsVariable) {
     throw new Error("how did we not have the variable in the scope?");
   }
 
   // We now need to begin tracing variable.references.
+  const matchingIdentifiers: TSESTree.Identifier[] = paramAsVariable.references.filter(
+    ref => ref.isRead()
+  ).map(
+    ref => ref.identifier as TSESTree.Identifier
+  );
 
   throw new Error("not yet implemented");
 }
@@ -91,9 +104,9 @@ await holdsArgument(
 );
 */
 
-await holdsArgument(
-  "_03_objectgraph_handlers/source/RevokerManagement.ts",
-  "RevokerManagement",
-  "addRevoker",
-  "owner"
-);
+await holdsArgument({
+  pathToTypeScriptFile: "_03_objectgraph_handlers/source/RevokerManagement.ts",
+  className: "RevokerManagement",
+  methodName: "addRevoker",
+  paramName: "owner"
+});
