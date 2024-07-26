63f6e5d7112a28484837861cfa981db3c12d2129
Alexander J. Vincent <ajvincent@gmail.com>
2024-07-25 22:09:03 -0700

    RevokedInFlight for the mirror object graph handler.

--- _04_mirror_membranes/source/MirrorObjectGraphHandler.ts
+++ _04_mirror_membranes/source/MirrorObjectGraphHandler.ts
@@ -1,10 +1,12 @@
 import ObjectGraphTailHandler from "#objectgraph_handlers/source/generated/ObjectGraphTailHandler.js";
+import RevokedInFlight from "#objectgraph_handlers/source/generated/decorators/revokedInFlight.js";
 import WrapReturnValues from "#objectgraph_handlers/source/generated/decorators/wrapReturnValues.js";
 import UpdateShadowTarget from "#objectgraph_handlers/source/decorators/updateShadowTarget.js";
 
+@RevokedInFlight
 @UpdateShadowTarget
 @WrapReturnValues
 export default
 class MirrorObjectGraphHandler extends ObjectGraphTailHandler {
-
+  // this class definition empty on purpose
 }
