9c0fce679c5d3c087589e43cb257c3943d862286
Alexander J. Vincent <ajvincent@gmail.com>
2025-02-02 13:19:20 -0800

    remove disabled code block

--- projects/reference-tracking/source/engine262-tools/defineBuiltInFunction.ts
+++ projects/reference-tracking/source/engine262-tools/defineBuiltInFunction.ts
@@ -15,25 +15,6 @@ export function defineBuiltInFunction(
     thisAndNewValue: { thisValue: GuestEngine.Value, newTarget: GuestEngine.Value }
   ): GuestEngine.Value | GuestEngine.ThrowCompletion
   {
-    /*
-    const guestReportedValues = guestArguments[0];
-    const guestValues: GuestEngine.Value[] = [];
-    if (guestReportedValues.type !== "Object") {
-      GuestEngine.Throw('TypeError', "NotAnObject", guestReportedValues);
-      return GuestEngine.Value.undefined;
-    }
-    if (!GuestEngine.isArrayExoticObject(guestReportedValues)) {
-      GuestEngine.Throw('TypeError', "NotATypeObject", guestReportedValues, "Array");
-      return GuestEngine.Value.undefined;
-    }
-
-    const length: number = GuestEngine.LengthOfArrayLike(guestArguments);
-    for (let index = 0; index < length; index++) {
-      const key: GuestEngine.JSStringValue = GuestEngine.Value(index.toString());
-      guestValues.push(GuestEngine.GetV(guestReportedValues, key));
-    }
-    */
-
     try {
       return callback(thisAndNewValue.thisValue, guestArguments, thisAndNewValue.newTarget);
     }
