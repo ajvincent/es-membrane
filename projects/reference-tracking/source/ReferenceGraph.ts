91f08a43e025101373ad15e798e8fe9d56d46ab0
Alexander J. Vincent <ajvincent@gmail.com>
2025-02-02 21:30:01 -0800

    String enums are easier to read than code numbers.

--- projects/reference-tracking/source/ReferenceGraph.ts
+++ projects/reference-tracking/source/ReferenceGraph.ts
@@ -1,40 +1,39 @@
 export enum ChildReferenceEdgeType {
-  PropertyName = 0,
-  ArrayIndex,
-  PropertySymbol,
-  PrivateClassField,
+  PropertyName = "PropertyName",
+  ArrayIndex = "ArrayIndex",
+  PropertySymbol = "PropertySymbol",
+  PrivateClassField = "PrivateClassField",
 
   // probably going to replace this with enums specific to each builtin class
-  InternalSlot,
+  InternalSlot = "InternalSlot",
 
   // less sure of these
-  FunctionClosure,
-  BoundFunction,
+  FunctionClosure = "FunctionClosure",
+  BoundFunction = "BoundFunction",
 
   // shortcut: foo.bar.baz[0] = wop;
-  PropertySequence,
+  PropertySequence = "PropertySequence",
 }
 
 export const TARGET_NODE_KEY = 0;
 export const PRESUMED_HELD_NODE_KEY = 1;
 
-export type BuiltInClassName = (
-  "Object" |
-  "Array" |
-  "Function" |
-  "WeakRef" |
-  "WeakMap" |
-  "WeakSet" |
-  "Map" |
-  "Set" |
-  "Proxy" |
-  "FinalizationRegistry" |
-  never
-);
+export enum BuiltInCollectionName {
+  Object = "Object",
+  Array = "Array",
+  Function = "Function",
+  WeakRef = "WeakRef",
+  WeakMap = "WeakMap",
+  WeakSet = "WeakSet",
+  Map = "Map",
+  Set = "Set",
+  Proxy = "Proxy",
+  FinalizationRegistry = "FinalizationRegistry",
+};
 
 export interface ReferenceGraphNode {
   readonly objectKey: number;
-  readonly builtInClassName: BuiltInClassName;
+  readonly builtInCollectionName: BuiltInCollectionName;
   readonly derivedClassName?: string;
 }
 
