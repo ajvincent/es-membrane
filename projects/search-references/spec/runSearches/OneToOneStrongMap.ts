63929516dd455da3c3a47663e09ad33a263d4a21
Alexander J. Vincent <ajvincent@gmail.com>
2025-05-30 22:46:04 -0700

    Enable OneToOneStrongMap integration test.

--- projects/search-references/spec/runSearches/OneToOneStrongMap.ts
+++ projects/search-references/spec/runSearches/OneToOneStrongMap.ts
@@ -1,34 +1,25 @@
 import {
   getActualGraph,
-  getTracingLog,
 } from "../support/getActualGraph.js";
 
-xdescribe("Integration test: OneToOneStrongMap,", () => {
+describe("Integration test: OneToOneStrongMap,", () => {
   it("proxied redArray via blueOwner is reachable", async () => {
     const actual = await getActualGraph("OneToOneStrongMap.js", "proxied redArray via blueOwner", false);
-    const log = getTracingLog("OneToOneStrongMap.js", "proxied redArray via blueOwner");
-    void(log);
     expect(actual).not.toBeNull();
   });
 
   it("revoked redArray via blueOwner", async () => {
-    const actual = await getActualGraph("OneToOneStrongMap.js", "proxied redArray via blueOwner", false);
-    const log = getTracingLog("OneToOneStrongMap.js", "revoked redArray via blueOwner");
-    void(log);
+    const actual = await getActualGraph("OneToOneStrongMap.js", "revoked redArray via blueOwner", false);
     expect(actual).toBeNull();
   });
 
   it("redArrayProxy via blueOwner", async () => {
-    const actual = await getActualGraph("OneToOneStrongMap.js", "proxied redArray via blueOwner", false);
-    const log = getTracingLog("OneToOneStrongMap.js", "redArrayProxy via blueOwner");
-    void(log);
-    expect(actual).toBeNull();
+    const actual = await getActualGraph("OneToOneStrongMap.js", "redArrayProxy via blueOwner", false);
+    expect(actual).not.toBeNull();
   });
 
   it("redOwnerProxy via blueOwner", async () => {
-    const actual = await getActualGraph("OneToOneStrongMap.js", "proxied redArray via blueOwner", false);
-    const log = getTracingLog("OneToOneStrongMap.js", "redOwnerProxy via blueOwner");
-    void(log);
-    expect(actual).toBeNull();
+    const actual = await getActualGraph("OneToOneStrongMap.js", "redOwnerProxy via blueOwner", false);
+    expect(actual).not.toBeNull();
   });
 });
