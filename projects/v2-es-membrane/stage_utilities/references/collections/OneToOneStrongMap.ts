cf97b973d2389ab6295c7699533b27606aae87cb
Alexander J. Vincent <ajvincent@gmail.com>
2026-05-26 20:12:32 -0700

    run search-references on OneToOneStrongMap

--- projects/v2-es-membrane/stage_utilities/references/collections/OneToOneStrongMap.ts
+++ projects/v2-es-membrane/stage_utilities/references/collections/OneToOneStrongMap.ts
@@ -20,7 +20,6 @@ testMap.bindOneToOne(firstGraphKey, firstValue, secondGraphKey, secondValue);
 searchReferences("1:1 binding to first value from just the map (weak)", firstValue, [testMap], false);
 searchReferences("1:1 binding to first value from just the map (strong)", firstValue, [testMap], true);
 
-searchReferences("1:1 binding to first value from the map and second value (weak)", firstValue, [testMap, secondValue], false);
 searchReferences("1:1 binding to first value from the map and second value (strong)", firstValue, [testMap, secondValue], true);
 
 testMap.delete(secondValue, secondGraphKey);
--- projects/v2-es-membrane/stage_utilities/spec/garbage-collection/collections/OneToOneStrongMap.ts
+++ projects/v2-es-membrane/stage_utilities/spec/garbage-collection/collections/OneToOneStrongMap.ts
@@ -6,7 +6,7 @@ import {
 
 import { stageDir } from "#stage_utilities/spec/support/stageDir.js";
 
-xdescribe("OneToOneStrongMap holds references to a value", () => {
+describe("OneToOneStrongMap holds references to a value", () => {
   let graphs: ReadonlyMap<string, SearchGraph | null>;
   beforeAll(async () => {
     graphs = await runSearchesInGuestEngine(
@@ -14,7 +14,16 @@ xdescribe("OneToOneStrongMap holds references to a value", () => {
     );
   });
 
-  it("todo", () => {
-    fail("not ready yet");
+  it("weakly from the map", () => {
+    expect(graphs.get("1:1 binding to first value from just the map (weak)")!).not.toBeNull();
+    expect(graphs.get("1:1 binding to first value from just the map (strong)")!).toBeNull();
+  });
+
+  it("jointly from the second value and the map", () => {
+    expect(graphs.get("1:1 binding to first value from the map and second value (strong)")!).not.toBeNull();
+  });
+
+  it("unless the second value was deleted", () => {
+    expect(graphs.get("1:1 binding after value was deleted")!).toBeNull();
   });
 });
