d10be351cb5cf4611e135c23eee235d1d221b5d0
Alexander J. Vincent <ajvincent@gmail.com>
2026-07-04 14:03:00 -0700

    membrane concepts, part 3.

--- projects/v2-es-membrane/membranes_decorated/spec/concepts.ts
+++ projects/v2-es-membrane/membranes_decorated/spec/concepts.ts
@@ -7,7 +7,7 @@ import type {
   MembraneIfc
 } from "../source/types/MembraneIfc.js";
 
-import {
+import type {
   MockDocumentIfc,
   MockElementIfc,
   MockEventIfc,
@@ -277,6 +277,534 @@ describe("basic concepts (with two object graphs): ", () => {
     expect(wetDocument.rootElement.firstChild).not.toBeNull();
     expect(element === wetDocument.rootElement.firstChild).toBeFalse();
     expect(element.name).toBe((wetDocument.rootElement.firstChild as MockElementIfc).name);
+  });
+
+  it("constructors work", () => {
+    const elem = new ElementDry(dryDocument, "test-element");
+    expect(elem.name).toBe("test-element");
+    expect(elem.ownerDocument === dryDocument).toBeTrue();
+    expect(Reflect.getPrototypeOf(elem) === ElementDry.prototype).toBeTrue();
+  });
+
+  // XXX ajvincent Be sure to retest this via frames, sandboxes.
+  it(
+    "Executing a function via .apply() returns a properly wrapped object",
+    function() {
+      const method1 = dryDocument.createElement;
+      const elem: MockElementIfc = method1.apply(dryDocument, ["test-element"]);
+      expect(elem.name).toBe("test-element");
+      expect(elem.ownerDocument === dryDocument).toBeTrue();
+    }
+  );
+
+  it("Looking up Object.isExtensible() works", function() {
+    let wetExtensible = Object.isExtensible(wetDocument);
+    let dryExtensible = Object.isExtensible(dryDocument);
+
+    expect(wetExtensible).toBeTrue();
+    expect(dryExtensible).toBeTrue();
+
+    // there's a later test for Object.preventExtension(dryDocument).
+    Object.preventExtensions(wetDocument);
+
+    wetExtensible = Object.isExtensible(wetDocument);
+    dryExtensible = Object.isExtensible(dryDocument);
+
+    expect(wetExtensible).toBeFalse();
+    expect(dryExtensible).toBeFalse();
+  });
+
+  it("Reflect.ownKeys() returns the same results for both object graphs with no distortions", () => {
+    expect(Reflect.ownKeys(dryDocument)).toEqual(Reflect.ownKeys(wetDocument));
+    expect(Reflect.ownKeys(dryDocument.rootElement)).toEqual(Reflect.ownKeys(wetDocument.rootElement));
+    expect(Reflect.ownKeys(ElementDry)).toEqual(Reflect.ownKeys(ElementWet));
+    expect(Reflect.ownKeys(ElementDry.prototype)).toEqual(Reflect.ownKeys(ElementWet.prototype));
+    expect(Reflect.ownKeys(NodeDry)).toEqual(Reflect.ownKeys(NodeWet));
+    expect(Reflect.ownKeys(NodeDry.prototype)).toEqual(Reflect.ownKeys(NodeWet.prototype));
+  });
+
+  it("The in operator works", function() {
+    function checkHas(
+      this: null,
+      value: object,
+      valueName: string,
+      propName: string,
+      index: number,
+      array: unknown[]
+    )
+    {
+      expect(propName in value).withContext(valueName + ":" + propName).toBe(index !== array.length - 1);
+    };
+    const propList = [
+      "nodeType",
+      "childNodes",
+      "ownerDocument",
+      "unknownProperty"
+    ];
+
+    propList.forEach(checkHas.bind(null, dryDocument, "dryDocument"));
+
+    // root follows inheritance patterns.
+    const root = dryDocument.rootElement;
+    propList.forEach(checkHas.bind(null, root, "root"));
+  });
+
+  describe("The delete operator works as expected", function() {
+    it("on dryDocument.rootElement", function() {
+      const wasDeleted = delete (dryDocument as Partial<MockDocumentIfc>).rootElement;
+      expect(typeof dryDocument.rootElement).toBe("undefined");
+      expect("rootElement" in dryDocument).toBeFalse();
+      expect(wasDeleted).toBeTrue();
+
+      expect("rootElement" in wetDocument).toBeFalse();
+    });
+
+    it("on dryDocument.rootElement.nodeName", function() {
+      const root = dryDocument.rootElement as { name?: string };
+      const wasDeleted = delete root.name;
+      expect(typeof root.name).toBe("undefined");
+      expect("nodeName" in root).toBeFalse();
+      expect(wasDeleted).toBeTrue();
+
+      expect(wetDocument.rootElement.name).toBeUndefined();
+    });
+
+    it("on dryDocument.rootElement.insertBefore", function() {
+      const root = dryDocument.rootElement as Partial<MockElementIfc>;
+      const wasDeleted = delete root.insertBefore;
+
+      // This is because insertBefore is inherited from NodeWet.prototype.
+      expect(typeof root.insertBefore).toBe("function");
+      expect("insertBefore" in root).toBeTrue();
+      expect(wasDeleted).toBeTrue();
+
+      expect(typeof wetDocument.rootElement.insertBefore).toBe("function");
+    });
+  });
+
+  describe("Deleting a property via Reflect.deleteProperty(...) works as expected", function() {
+    it("when the property doesn't exist", function() {
+      expect(Reflect.deleteProperty(dryDocument, "doesNotExist")).toBeTrue();
+    });
+
+    it("when the property descriptor has configurable: true", function() {
+      Reflect.defineProperty(dryDocument, "doesNotExist", {
+        value: 2,
+        writable: true,
+        enumerable: true,
+        configurable: true
+      });
+      expect(Reflect.deleteProperty(dryDocument, "doesNotExist")).toBeTrue();
+      expect(Reflect.getOwnPropertyDescriptor(dryDocument, "doesNotExist")).toBeUndefined();
+    });
+
+    it("when the property descriptor has configurable: false", function() {
+      Reflect.defineProperty(dryDocument, "doesNotExist", {
+        value: 2,
+        writable: true,
+        enumerable: true,
+        configurable: false
+      });
+      expect(Reflect.deleteProperty(dryDocument, "doesNotExist")).toBeFalse();
+      const desc = Reflect.getOwnPropertyDescriptor(dryDocument, "doesNotExist");
+      expect(typeof desc).toBe("object");
+      if (desc) {
+        expect(desc.value).toBe(2);
+      }
+    });
+
+    it(
+      "when the property descriptor is initially defined on the original target with configurable: true",
+      function() {
+        Reflect.defineProperty(wetDocument, "doesNotExist", {
+          value: 2,
+          writable: true,
+          enumerable: true,
+          configurable: true
+        });
+        expect(Reflect.deleteProperty(dryDocument, "doesNotExist")).toBeTrue();
+        expect(
+          Reflect.getOwnPropertyDescriptor(dryDocument, "doesNotExist")
+        ).toBeUndefined();
+      }
+    );
+
+    it(
+      "when the property descriptor is initially defined on the original target with configurable: false",
+      function() {
+        Reflect.defineProperty(wetDocument, "doesNotExist", {
+          value: 2,
+          writable: true,
+          enumerable: true,
+          configurable: false
+        });
+        expect(Reflect.deleteProperty(dryDocument, "doesNotExist")).toBeFalse();
+        const desc = Reflect.getOwnPropertyDescriptor(dryDocument, "doesNotExist");
+        expect(typeof desc).toBe("object");
+        if (desc) {
+          expect(desc.value).toBe(2);
+        }
+      }
+    );
+  });
+
+  interface DocProperties extends MockDocumentIfc {
+    screenWidth?: number;
+    screenHeight?: number;
+    location?: Record<"name", string>;
+    extra?: object;
+  }
+
+  it("Defining a property via Object.defineProperty(...) works as expected", function() {
+    const dryDoc = dryDocument as DocProperties;
+    const wetDoc = wetDocument as DocProperties;
+
+    Object.defineProperty(dryDoc, "screenWidth", {
+      value: 200,
+      writable: true,
+      enumerable: true,
+      configurable: true
+    });
+    expect(dryDoc.screenWidth).toBe(200);
+    expect(wetDoc.screenWidth).toBe(200);
+
+    let localHeight: number = 150;
+    Object.defineProperty(dryDocument, "screenHeight", {
+      get: function() { return localHeight; },
+      set: function(val: number) { localHeight = val; },
+      enumerable: true,
+      configurable: true
+    });
+    expect(dryDoc.screenHeight).toBe(150);
+    expect(wetDoc.screenHeight).toBe(150);
+
+    const location: Record<"name", string> = {
+      name: "location"
+    };
+    Object.defineProperty(dryDocument, "location", {
+      value: location,
+      writable: true,
+      enumerable: true,
+      configurable: true
+    });
+    expect(dryDoc.location === location).toBe(true);
+    expect(wetDoc.location !== location).toBe(true);
+    expect(wetDoc.location!.name === "location").toBe(true);
+
+    /* XXX ajvincent There is an obvious temptation to just call:
+     * dryDocument.screenWidth = 200;
+     *
+     * That's covered in the next test.  Here, we're testing defineProperty.
+     *
+     * On the other hand, we've just tested that setting a property from the
+     * "dry" side retains its identity with the "dry" object graph.
+     */
+
+    // Additional test for configurable: false
+    const obj = {};
+    Object.defineProperty(dryDoc, "extra", {
+      value: obj,
+      writable: true,
+      enumerable: false,
+      configurable: false
+    });
+    const extra = dryDoc.extra;
+    expect(extra).toBe(obj);
+  });
+
+  it("Defining a property directly works as expected", function() {
+    const dryDoc = dryDocument as DocProperties;
+    const wetDoc = wetDocument as DocProperties;
+
+    dryDoc.screenWidth = 200;
+    expect(dryDoc.screenWidth).toBe(200);
+    expect(wetDoc.screenWidth).toBe(200);
+
+    let localHeight: number = 150;
+    Object.defineProperty(dryDocument, "screenHeight", {
+      get: function() { return localHeight; },
+      set: function(val: number) { localHeight = val; },
+      enumerable: true,
+      configurable: true
+    });
+    wetDoc.screenHeight = 200;
+    expect(dryDoc.screenHeight).toBe(200);
+    expect(wetDoc.screenHeight).toBe(200);
+
+    const location = {
+      name: "location"
+    };
+    dryDoc.location = location;
+    expect(dryDoc.location === location).toBe(true);
+    expect(wetDoc.location !== location).toBe(true);
+    expect(wetDoc.location!.name === "location").toBe(true);
+  });
+
+  it("Setting a prototype works as expected", function() {
+    interface ElementWithNamespace extends MockElementIfc {
+      namespaceURI?: string;
+    }
+    const XHTMLElementDryProto = Object.create(ElementDry.prototype, {
+      namespaceURI: {
+        value: "http://www.w3.org/1999/xhtml",
+        writable: false,
+        enumerable: true,
+        configurable: true,
+      }
+    }) as ElementWithNamespace;
+
+    class TraceMap extends Map<object, string> {
+      addMember(value: object, name: string): void {
+        this.getOrInsert(value, name);
+        if (typeof value === "function")
+          this.getOrInsert(value.prototype as object, name + ".prototype");
+      }
+
+      getPrototypeChain(value: object) {
+        const rv: string[] = [];
+        let next: string;
+        while (value) {
+          next = this.get(value) || "(unknown)";
+          rv.push(next);
+
+          // yes, getPrototypeOf can return null.  The while loop takes care of this.
+          value = Reflect.getPrototypeOf(value)!;
+        }
+        return rv;
+      }
+    }
+
+    const traceMap = new TraceMap();
+    {
+      traceMap.addMember(Reflect.getPrototypeOf(dryDocument)!.constructor, "DocumentDry");
+      traceMap.addMember(dryDocument, "dryDocument");
+      traceMap.set(dryDocument.rootElement, "dryDocument.rootElement");
+      traceMap.addMember(ElementDry, "ElementDry");
+      traceMap.addMember(NodeDry, "NodeDry");
+      traceMap.addMember(Reflect.getPrototypeOf(NodeDry.prototype)!.constructor, "EventTargetDry");
+
+      traceMap.addMember(Reflect.getPrototypeOf(wetDocument)!.constructor, "DocumentWet");
+      traceMap.addMember(wetDocument, "wetDocument");
+      traceMap.set(wetDocument.rootElement, "wetDocument.rootElement");
+      traceMap.addMember(ElementWet, "ElementWet");
+      traceMap.addMember(NodeWet, "NodeWet");
+      traceMap.addMember(Reflect.getPrototypeOf(NodeWet.prototype)!.constructor, "EventTargetWet");
+
+      traceMap.set(XHTMLElementDryProto, "XHTMLElementDryProto");
+    }
+
+    {
+      const chain = traceMap.getPrototypeChain(wetDocument.rootElement);
+      const expectedChain = [
+        "wetDocument.rootElement",
+        "ElementWet.prototype",
+        "NodeWet.prototype",
+        "EventTargetWet.prototype",
+        "(unknown)"
+      ];
+      expect(chain).toEqual(expectedChain);
+    }
+
+    {
+      const chain = traceMap.getPrototypeChain(dryDocument.rootElement);
+      const expectedChain = [
+        "dryDocument.rootElement",
+        "ElementDry.prototype",
+        "NodeDry.prototype",
+        "EventTargetDry.prototype",
+        "(unknown)"
+      ];
+      expect(chain).toEqual(expectedChain);
+    }
+
+    {
+      const chain = traceMap.getPrototypeChain(XHTMLElementDryProto);
+      const expectedChain = [
+        "XHTMLElementDryProto",
+        "ElementDry.prototype",
+        "NodeDry.prototype",
+        "EventTargetDry.prototype",
+        "(unknown)"
+      ];
+      expect(chain).toEqual(expectedChain);
+    }
+
+    const dryRoot = dryDocument.rootElement;
+    expect(Reflect.setPrototypeOf(dryRoot, XHTMLElementDryProto)).toBe(true);
+    expect(Reflect.getPrototypeOf(dryRoot) === XHTMLElementDryProto).toBe(true);
+    {
+      const chain = traceMap.getPrototypeChain(dryRoot);
+      const expectedChain = [
+        "dryDocument.rootElement",
+        "XHTMLElementDryProto",
+        "ElementDry.prototype",
+        "NodeDry.prototype",
+        "EventTargetDry.prototype",
+        "(unknown)"
+      ];
+      expect(chain).toEqual(expectedChain);
+    }
+
+    const wetRoot = wetDocument.rootElement;
+    {
+      const chain = traceMap.getPrototypeChain(wetRoot);
+      const expectedChain = [
+        "wetDocument.rootElement",
+        "(unknown)",
+        "ElementWet.prototype",
+        "NodeWet.prototype",
+        "EventTargetWet.prototype",
+        "(unknown)"
+      ];
+      expect(chain).toEqual(expectedChain);
+    }
+
+    expect((dryRoot as ElementWithNamespace).namespaceURI).toBe(XHTMLElementDryProto.namespaceURI);
+    expect((wetRoot as ElementWithNamespace).namespaceURI).toBe(XHTMLElementDryProto.namespaceURI);
+
+    /* Class prototypes are not writable.
+    class XHTMLElementDry extends ElementDry {
+    }
+    XHTMLElementDry.prototype = XHTMLElementDryProto; // throws an error
+
+    Of course people can sort of hack around it...
+    */
+    const XHTMLElementDry = class extends ElementDry {
+      constructor(ownerDoc: MockDocumentIfc, name: string) {
+        super(ownerDoc, name);
+        Reflect.setPrototypeOf(this, XHTMLElementDryProto);
+      }
+    } as Class<ElementWithNamespace, [MockDocumentIfc, string]>;
+    traceMap.addMember(XHTMLElementDry, "XHTMLElementDry");
+
+    const x = new XHTMLElementDry(dryDocument, "test-element");
+    traceMap.addMember(x, "x");
+
+    {
+      const chain = traceMap.getPrototypeChain(x);
+      const expectedChain = [
+        "x",
+        "XHTMLElementDryProto",
+        "ElementDry.prototype",
+        "NodeDry.prototype",
+        "EventTargetDry.prototype",
+        "(unknown)"
+      ];
+      expect(chain).toEqual(expectedChain);
+    }
+    expect(x.namespaceURI).toBe(XHTMLElementDryProto.namespaceURI);
+    expect(x.nodeType).toBe(1);
+  });
+
+  it("Calling Object.preventExtensions(...) works as expected", function() {
+    expect(Object.isExtensible(dryDocument)).toBe(true);
+    Object.preventExtensions(dryDocument);
+    expect(Object.isExtensible(dryDocument)).toBe(false);
+
+    // this line is NOT expected to throw an exception
+    Object.preventExtensions(dryDocument);
+    expect(Object.isExtensible(dryDocument)).toBe(false);
+  });
+
+  it("Array.prototype.splice works on wrapped arrays", function() {
+    interface StringsOwner extends MockDocumentIfc {
+      strings: string[];
+    }
+    const wetDoc = wetDocument as StringsOwner;
+    const dryDoc = dryDocument as StringsOwner;
+
+    wetDoc.strings = ["alpha", "beta", "gamma"];
+    expect(dryDoc.strings.length).toBe(3);
+
+    Array.prototype.splice.apply(dryDoc.strings, [
+      1, 1, "delta", "epsilon"
+    ]);
+
+    expect(wetDoc.strings).toEqual(["alpha", "delta", "epsilon", "gamma"]);
+  });
+
+  describe("Receivers in proxies", function() {
+    interface Alphabet {
+      ALPHA: Record<"value", "A">,
+      BETA: Record<"value", "B">,
+      alpha: {
+        _hidden: Record<"value", string>,
+        upper: Record<"value", string>,
+        value: "a"
+      },
+      beta: {
+        _hidden: Record<"value", string>,
+        upper: Record<"value", string>,
+        value: "b"
+      }
+
+      X: Record<"value", string>
+    }
+
+    let wetObj: Alphabet, dryObj: Alphabet;
+    beforeEach(function() {
+      const ALPHA: Record<"value", "A"> = {
+        value: "A"
+      };
+      const BETA: Record<"value", "B"> = {
+        value: "B"
+      };
+
+      const alpha: {
+        _hidden: Record<"value", string>,
+        upper: Record<"value", string>,
+        value: "a"
+      } = {
+        get upper() {
+          return this._hidden;
+        },
+        set upper(val) {
+          this._hidden = val;
+        },
+        _hidden: ALPHA,
+        value: "a",
+      };
+
+      const beta: {
+        _hidden: Record<"value", string>,
+        upper: Record<"value", string>,
+        value: "b"
+      } = {
+        _hidden: { value: "F" },
+        upper: BETA,
+        value: "b"
+      };
+
+      wetObj = {
+        ALPHA,
+        BETA,
+        alpha,
+        beta,
+
+        X: { value: "X" },
+      };
+
+      dryObj = membrane.convertObject(
+        "wet", "dry", wetObj
+      );
+    });
+
+    it("are where property lookups happen", function() {
+      const dry_a = dryObj.alpha, dry_b = dryObj.beta;
+      const val = Reflect.get(dry_a, "upper", dry_b);
+      expect(val).toBe(dry_b._hidden);
+    });
+
+    it("are where property setter invocations happen", function() {
+      const dry_a = dryObj.alpha, dry_b = dryObj.beta, dryA = dryObj.ALPHA, dryX = dryObj.X;
+      const wetX = wetObj.X;
+
+      Reflect.set(dry_a, "upper", dryX, dry_b);
+      expect(dry_b._hidden).toBe(dryX);
+      expect(dry_a._hidden).toBe(dryA);
+
+      expect(wetObj.beta._hidden).toBe(wetX);
+    });
   });
 
   describe("Revocation works", () => {
